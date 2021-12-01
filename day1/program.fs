7acb8d27890300e6083fa79525f8ece01c33edac
Fyodor Shchukin <ted888@ya.ru>
2021-12-01 23:18:23 +0300

    day1

---
+++ day1/program.fs
@@ -0,0 +1,31 @@
+module program
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+    let numbers =
+        File.ReadLines @"input"
+        |> Seq.map Int32.Parse
+
+    let countRises numbers =
+        let rises =
+            Seq.windowed 2 numbers
+            |> Seq.map (fun c -> c[0] < c[1])
+
+        Seq.where id rises |> Seq.length
+
+    let ``part one`` =
+        numbers
+        |> countRises
+
+    let ``part two`` =
+        Seq.windowed 3 numbers
+        |> Seq.map (Array.reduce (+))
+        |> countRises
+
+    printfn $"Answer for part one is: {``part one``}"
+    printfn $"Answer for part two is: {``part two``}"
+
+    0
