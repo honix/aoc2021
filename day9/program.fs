e6f4f35a14e25efeb01722d75c52f0383a6e79b3
Fyodor Shchukin <ted888@ya.ru>
2021-12-10 17:11:08 +0300

    day9 two stars

--- day9/program.fs
+++ day9/program.fs
@@ -4,7 +4,7 @@ open System
 open System.IO
 open FParsec
 
-let parse text =
+let parse text : (int list) list =
     let tile = digit |>> (sprintf "%c" >> Int32.Parse)
     let row = many1 tile
     let grid = sepEndBy row newline .>> eof
@@ -13,35 +13,76 @@ let parse text =
     | Success (result, _, _) -> result
     | Failure (error, _, _) -> failwith error
 
+let closes (grid: int [,]) (x, y) : (int * int) list =
+    let mutable closes = []
+
+    if x - 1 >= 0 then
+        closes <- (x - 1, y) :: closes
+
+    if x + 1 < Array2D.length2 grid then
+        closes <- (x + 1, y) :: closes
+
+    if y - 1 >= 0 then
+        closes <- (x, y - 1) :: closes
+
+    if y + 1 < Array2D.length1 grid then
+        closes <- (x, y + 1) :: closes
+
+    closes
+
+let islandSize (grid: int [,]) (x, y) : int =
+    let visitedCells = set [ (x, y) ]
+
+    let rec islandSizeRec visitedCells =
+        let closes =
+            Set.fold (fun s coord -> Set.union s (set (closes grid coord))) Set.empty visitedCells
+
+        let front =
+            Set.difference closes visitedCells
+            |> Set.filter (fun (x, y) -> Array2D.get grid y x <> 9)
+
+        match front with
+        | x when x.IsEmpty -> visitedCells
+        | x -> islandSizeRec (Set.union visitedCells x)
+
+    islandSizeRec visitedCells
+    |> Set.toList
+    |> List.length
+
+let minCoords (grid: int [,]) : (int * int) list =
+    grid
+    |> Array2D.mapi (fun y x v -> v, closes grid (x, y))
+    |> fun grid' ->
+        let mutable acc = []
+
+        Array2D.iteri
+            (fun y x (v, closes) ->
+                if List.forall (fun (x, y) -> v < Array2D.get grid y x) closes then
+                    acc <- (x, y) :: acc)
+            grid'
+
+        acc
+
+
 [<EntryPoint>]
 let main _ =
     let text = File.ReadAllText @"input"
     let grid = parse text |> array2D
 
-    let n =
-        Array2D.mapi
-            (fun y x v ->
-                let mutable closes = []
+    let minCoords = minCoords grid
 
-                if x - 1 >= 0 then
-                    closes <- grid.[y, x - 1] :: closes
+    let answerOne =
+        List.fold (fun s (x, y) -> s + (Array2D.get grid y x) + 1) 0 minCoords
 
-                if x + 1 < Array2D.length2 grid then
-                    closes <- grid.[y, x + 1] :: closes
+    printfn $"Answer for part one is: {answerOne}"
 
-                if y - 1 >= 0 then
-                    closes <- grid.[y - 1, x] :: closes
+    let answerTwo =
+        minCoords
+        |> List.map (islandSize grid)
+        |> List.sortDescending
+        |> List.take 3
+        |> List.reduce (*)
 
-                if y + 1 < Array2D.length1 grid then
-                    closes <- grid.[y + 1, x] :: closes
-
-                List.forall ((<) v) closes, v)
-            grid
-        |> (fun grid ->
-            let mutable n = 0
-            Array2D.iter (fun (b, v) -> if b then n <- n + v + 1) grid
-            n)
-
-    printfn $"%A{n}"
+    printfn $"Answer for part two is: {answerTwo}"
 
     0
